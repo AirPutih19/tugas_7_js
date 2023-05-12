c6abd0ea3703e5094d83d383fa66d1c4fa0581af
AirPutih19 <kharismadwisj@gmail.com>
2023-05-12 13:13:25 +0700

    Tugas 7 JS
    Tugas 7 JS untuk niomic

---
+++ tugas7.js
@@ -0,0 +1,7 @@
+var tinggiBadan = ["a = 160cm", "b = 165cm ", "c = 155cm", "d = 170cm"]
+    for (i=0;i<tinggiBadan.length;i++){
+        console.log(tinggiBadan[i]);
+    } console.log("")
+    for(let x of tinggiBadan){
+        console.log(x);
+    } console.log("Terdapat perbedaan yaitu ketika menggunakan for of tidak perlu menuliskan index sehingga lebih simple ")
